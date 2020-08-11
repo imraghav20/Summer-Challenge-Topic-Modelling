c72306ccf6b79a83cc142ff11bce3261580e99d5
Abhishek Raghav <59257038+imraghav20@users.noreply.github.com>
2020-08-11 23:57:21 +0530

    Update settings.py

--- CoronaDiaries/settings.py
+++ CoronaDiaries/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '70^j97q5nr6q_q^=wo-!qj1-k22+*f2i786%s&%u*#v#qws$77'
+SECRET_KEY = 'SECRET-KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
